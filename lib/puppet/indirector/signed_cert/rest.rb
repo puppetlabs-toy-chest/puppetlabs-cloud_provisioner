13ab6fd2bed45ab3f1c16b3b9cae246fb2cecae3
Luke Kanies <luke@puppetlabs.com>
2011-03-22 10:51:39 -0700

    Moving SSL stuff to puppet-certificate-status
    Signed-off-by: Luke Kanies <luke@puppetlabs.com>

--- lib/puppet/indirector/signed_cert/rest.rb
+++
@@ -1,9 +0,0 @@
-require 'puppet/ssl/certificate'
-require 'puppet/indirector/rest'
-
-class Puppet::SSL::SignedCert::Rest < Puppet::Indirector::REST
-  desc "Sign certificate requests over HTTP via REST."
-
-  use_server_setting(:ca_server)
-  use_port_setting(:ca_port)
-end
