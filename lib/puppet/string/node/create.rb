f4a84eb567e901c3d92d4e431308a1697b42f940
Pieter van de Bruggen <pieter@puppetlabs.com>
2011-03-29 14:33:51 -0700

    (#6889) Basic Puppet bootstrapping.
    Invoking `puppet node bootstrap` with an appropriate set of options can
    now automatically create a new instance of the given AMI and successfully
    install PE on it.
    
    Paired-With: Nick Lewis
    Paired-With: Matt Robinson

---
+++ lib/puppet/string/node/create.rb
@@ -0,0 +1,51 @@
+require 'rubygems'
+require 'fog'
+
+Puppet::String.define :node, '0.0.1' do
+  action :create do
+    option '--image=', '-i='
+    option '--keypair=', '-k='
+    option '--group=', '-g=', '--security-group='
+    invoke do |name, options|
+      print "Connecting to AWS ..."
+      connection = Fog::Compute.new(:provider => 'AWS')
+      puts " Done"
+
+      print "Creating new instance ..."
+      server = connection.servers.create(
+        :image_id => options[:image],
+        :key_name => options[:keypair],
+        :groups   => (options[:group] || '').split(File::PATH_SEPARATOR)
+      )
+      puts " Done"
+
+      print "Starting up "
+      while server.state == 'pending'
+        print '.'
+        server.reload
+      end
+      puts " Done"
+
+      if server.state == 'running'
+        # TODO: Find a better way of getting the Fingerprints
+        print "Waiting to capture console output "
+        while server.console_output.body['output'].nil?
+          print '.'
+          sleep 2
+        end
+        puts ' Done'
+        puts
+
+        server.console_output.body['output'].each_line do |line|
+          puts line if line =~ /^ec2:/
+        end
+
+        puts "Running as: #{server.dns_name}"
+      else
+        puts "Failed: #{server.state_reason.inspect}"
+      end
+
+      server
+    end
+  end
+end
---
+++ lib/puppet/string/node/init.rb
@@ -0,0 +1,59 @@
+require 'rubygems'
+require 'fog'
+
+Puppet::String.define :node, '0.0.1' do
+  action :bootstrap do
+    option '--image=', '-i='
+    option '--keypair=', '-k='
+    option '--group=', '-g=', '--security-group='
+    option '--login=', '-l=', '--username'
+    option '--keyfile=', '-k='
+    option '--tarball=', '--puppet='
+    option '--answers='
+    invoke do |name, options|
+      server = self.create(nil, options)
+      self.init(nil, server, options)
+    end
+  end
+
+  action :init do
+    option '--login=', '-l=', '--username'
+    option '--keyfile=', '-k='
+    option '--tarball=', '--puppet='
+    option '--answers='
+    invoke do |name, server, options|
+      server.username = options['login']
+      server.private_key_path = options['keyfile']
+
+      print "Waiting for SSH response ..."
+      retries = 0
+      begin
+        # TODO: Certain cases cause this to hang?
+        server.ssh(['hostname'])
+      rescue
+        sleep 60
+        retries += 1
+        print '.'
+        raise "SSH not responding; aborting." if retries > 5
+        retry
+      end
+      puts " Done"
+
+      print "Uploading Puppet ..."
+      server.scp(options['tarball'], '/tmp/puppet.tar.gz')
+      puts " Done"
+
+      print "Uploading Puppet Answer File ..."
+      server.scp(option['answers'], '/tmp/puppet.answers')
+      puts " Done"
+
+      print "Installing Puppet ..."
+      steps = [
+        'tar -xvzf /tmp/puppet.tar.gz -C /tmp',
+        '/tmp/puppet-enterprise-1.0-all/puppet-enterprise-installer -a /tmp/puppet.answers'
+      ]
+      server.ssh(steps.map { |c| server.username == 'root' ? cmd : "sudo #{c}" })
+      puts " Done"
+    end
+  end
+end
