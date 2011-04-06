bc1160b942d4c63d5552461ee738ed4afa5a4384
Pieter van de Bruggen <pieter@puppetlabs.com>
2011-04-06 12:23:41 -0700

    (#7000) Segregate #init and #install actions.
    Reviewed-By: Nick Lewis

--- lib/puppet/string/node/init.rb
+++ lib/puppet/string/node/init.rb
@@ -24,6 +24,18 @@ Puppet::String.define :node, '0.0.1' do
     option '--tarball=', '--puppet='
     option '--answers='
     when_invoked do |server, options|
+      certname = install(server, options)
+      # TODO: Sign Certificate.
+      # TODO: Register / classify node with ENC.
+    end
+  end
+
+  action :install do
+    option '--login=', '-l=', '--username='
+    option '--keyfile=', '-k='
+    option '--tarball=', '--puppet='
+    option '--answers='
+    when_invoked do |server, options|
       login    = options[:login]
       keyfile  = options[:keyfile]
 
