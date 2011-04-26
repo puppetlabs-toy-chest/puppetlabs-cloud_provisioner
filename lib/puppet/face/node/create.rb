eb6f21bdf6bd2968d71c9676694b0efde335c83e
Max Martin <max@puppetlabs.com>
2011-04-26 14:53:11 -0700

    (#7248) CloudPack general refactor
    Move action options and definition from individual face action files
    into Puppet::CloudPack module. Also moved testing from individual face
    action spec files to CloudPack module spec.
    
    Paired-with: Pieter van de Bruggen <pieter@puppetlabs.com>

--- lib/puppet/face/node/create.rb
+++ lib/puppet/face/node/create.rb
@@ -1,96 +1,10 @@
-require 'rubygems'
-require 'fog'
+require 'puppet/cloudpack'
 
 Puppet::Face.define :node, '0.0.1' do
   action :create do
-    # TODO: Should mark certain options as required.
-    # TODO: Validate parameters.
-    option '--image=', '-i='
-    option '--keypair=', '-k='
-    option '--group=', '-g=', '--security-group='
+    Puppet::CloudPack.add_create_options(self)
     when_invoked do |options|
-      unless options.has_key? :_destroy_server_at_exit
-        options[:_destroy_server_at_exit] = :create
-      end
-
-      connection = create_connection()
-
-      # TODO: Validate that the security groups permit SSH access from here.
-      # TODO: Can this throw errors?
-      server     = create_server(connection.servers,
-        :image_id => options[:image],
-        :key_name => options[:keypair],
-        :groups   => (options[:group] || '').split(File::PATH_SEPARATOR)
-      )
-
-      Signal.trap(:EXIT) do
-        if options[:_destroy_server_at_exit]
-          server.destroy rescue nil
-        end
-      end
-
-      create_tags(connection.tags, server)
-
-      print 'Starting up '
-      retries = 0
-      begin
-        server.wait_for do
-          print '.'
-          self.ready?
-        end
-        puts ' Done'
-      rescue Fog::Errors::Error
-        puts "Failed"
-        Puppet.err "Could not connect to host"
-        Puppet.err "Please check your network connection and try again"
-        return nil
-      end
-
-      # TODO: Find a better way of getting the Fingerprints
-      begin
-        print 'Waiting for host fingerprints '
-        Fog.wait_for do
-          print '.'
-          not server.console_output.body['output'].nil?
-        end or raise Fog::Errors::Error, "Waiting for host fingerprints timed out"
-        puts ' Done'
-
-        puts *server.console_output.body['output'].grep(/^ec2:/)
-      rescue Fog::Errors::Error => e
-        puts "Failed"
-        Puppet.warning "Could not read the host's fingerprints"
-        Puppet.warning "Please verify the host's fingerprints through AWS"
-      end
-
-      if options[:_destroy_server_at_exit] == :create
-        options.delete(:_destroy_server_at_exit)
-      end
-
-      return server.dns_name
+      Puppet::CloudPack.create(options)
     end
-  end
-
-  def create_connection(options = { :provider => 'AWS' })
-    print 'Connecting to AWS ...'
-    connection = Fog::Compute.new(options)
-    puts ' Done'
-    return connection
-  end
-
-  def create_server(servers, options = {})
-    print 'Creating new instance ...'
-    server = servers.create(options)
-    puts ' Done'
-    return server
-  end
-
-  def create_tags(tags, server)
-    print 'Creating tags for instance ...'
-    tags.create(
-      :key         => 'Created-By',
-      :value       => 'Puppet',
-      :resource_id => server.id
-    )
-    puts ' Done'
   end
 end
--- lib/puppet/face/node/terminate.rb
+++ lib/puppet/face/node/terminate.rb
@@ -1,22 +1,10 @@
-require 'rubygems'
-require 'fog'
+require 'puppet/cloudpack'
 
 Puppet::Face.define :node, '0.0.1' do
   action :terminate do
-    option '--force', '-f'
+    Puppet::CloudPack.add_terminate_options(self)
     when_invoked do |server, options|
-      connection = create_connection()
-
-      servers = connection.servers.all('dns-name' => server)
-      if servers.length == 1 || options[:force]
-        servers.each { |server| server.destroy() }
-      elsif servers.empty?
-        Puppet.warning "Could not find server with DNS name '#{server}'"
-      else
-        Puppet.err "More than one server with DNS name '#{server}'; aborting"
-      end
-
-      nil
+      Puppet::CloudPack.terminate(server, options)
     end
   end
 end
