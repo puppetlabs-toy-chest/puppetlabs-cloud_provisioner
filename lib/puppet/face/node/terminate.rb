fa1ee3ab70284c5d712fe923760c338edf66cc41
Pieter van de Bruggen <pieter@puppetlabs.com>
2011-04-26 11:54:08 -0700

    (#7237) Add explicit `--force` flag.
    This change also means that `puppet node terminate` will avoid accidentally
    destroying more than one instance by default.
    
    Reviewed-By: Max Martin

--- lib/puppet/face/node/terminate.rb
+++ lib/puppet/face/node/terminate.rb
@@ -3,12 +3,17 @@ require 'fog'
 
 Puppet::Face.define :node, '0.0.1' do
   action :terminate do
+    option '--force', '-f'
     when_invoked do |server, options|
       connection = create_connection()
 
       servers = connection.servers.all('dns-name' => server)
-      servers.each do |server|
-        server.destroy()
+      if servers.length == 1 || options[:force]
+        servers.each { |server| server.destroy() }
+      elsif servers.empty?
+        Puppet.warning "Could not find server with DNS name '#{server}'"
+      else
+        Puppet.err "More than one server with DNS name '#{server}'; aborting"
       end
 
       nil
