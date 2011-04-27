f849c9fb9b67f485773ce4767978358ab2491738
Pieter van de Bruggen <pieter@puppetlabs.com>
2011-04-27 16:42:56 -0700

    (Maint.) Fix error in `puppet node classify`.
    Calling the `create` method with `classify`'s options strangely doesn't work well.
    
    Reviewed-By: Daniel Pittman

--- lib/puppet/face/node/classify.rb
+++ lib/puppet/face/node/classify.rb
@@ -4,7 +4,7 @@ Puppet::Face.define :node, '0.0.1' do
   action :classify do
     Puppet::CloudPack.add_classify_options(self)
     when_invoked do |certname, options|
-      Puppet::CloudPack.create(certname, options)
+      Puppet::CloudPack.classify(certname, options)
     end
   end
 end
