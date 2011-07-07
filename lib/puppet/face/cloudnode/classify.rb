383a9abb6c61c2357ccee8604076eadb2c683245
Dan Bode <dan@puppetlabs.com>
2011-07-07 15:05:07 -0700

    (#7510) Document actions and options
    This patch adds reasaonble documentations to 
    all actions and options.

--- lib/puppet/face/cloudnode/classify.rb
+++ lib/puppet/face/cloudnode/classify.rb
@@ -2,6 +2,17 @@ require 'puppet/cloudpack'
 
 Puppet::Face.define :cloudnode, '0.0.1' do
   action :classify do
+    summary 'Specify how Puppet should classify a node'
+    description <<-EOT
+      Make Puppet Dashboard aware of a newly created agent
+      node and add it to a node group, thus allowing it to
+      receive proper configurations on its next run. This
+      action will have no material effect unless you’re using
+      Puppet dashboard for node classification.
+
+      This action is not restricted to cloud machine instances.
+      It can be run multiple times for a single node.
+    EOT
     Puppet::CloudPack.add_classify_options(self)
     when_invoked do |certname, options|
       Puppet::CloudPack.classify(certname, options)
--- lib/puppet/face/cloudnode/install.rb
+++ lib/puppet/face/cloudnode/install.rb
@@ -2,6 +2,12 @@ require 'puppet/cloudpack'
 
 Puppet::Face.define :cloudnode, '0.0.1' do
   action :install do
+    summary 'Install Puppet on an arbitrary node'
+    description <<-EOT
+      Installs Puppet on an existing instance. It uses scp to
+      copy installation requirements to the machine and ssh to
+      run the installation commmands remotely.
+    EOT
     Puppet::CloudPack.add_install_options(self)
     when_invoked do |server, options|
       Puppet::CloudPack.install(server, options)
