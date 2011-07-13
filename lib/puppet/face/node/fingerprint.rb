b7d7a7f82b04f1bd06e5c885a389e6f4b1423a0b
Jeff McCune <jeff@puppetlabs.com>
2011-07-13 14:57:04 -0700

    (#8406) Rename cloudnode back to node
    This change renames the actions added since the changes introduced in
    ticket #8160 back to node from cloudnode.
    
    Reviewed-by: Dan Bode

--- lib/puppet/face/node/fingerprint.rb
+++ lib/puppet/face/node/fingerprint.rb
@@ -1,6 +1,6 @@
 require 'puppet/cloudpack'
 
-Puppet::Face.define :cloudnode, '0.0.1' do
+Puppet::Face.define :node, '0.0.1' do
   action :fingerprint do
     summary 'Make a best effort to securely obtain the SSH host key fingerprint'
     description <<-EOT
